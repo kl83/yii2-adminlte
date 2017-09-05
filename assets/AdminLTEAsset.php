343c79583f97cbc5908e4871ce1404a98693b3a1
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2017-09-05 17:48:54 +0300

    Added asset

---
+++ assets/AdminLTEAsset.php
@@ -0,0 +1,21 @@
+<?php
+namespace kl83\assets;
+
+class AdminLTEAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/admin-lte/dist';
+    public $depends = [
+        'yii\bootstrap\BootstrapAsset',
+    ];
+    public function init()
+    {
+        $min = YII_DEBUG ? '' : '.min';
+        $this->css = [
+            "css/AdminLTE$min.css",
+            "css/skins/_all-skins$min.css",
+        ];
+        $this->js = [
+            "js/app$min.js",
+        ];
+    }
+}
