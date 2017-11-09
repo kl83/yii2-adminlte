2b429a0cbd707a6f0eb690b2aff78a7e4bd4d85d
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2017-11-09 22:26:56 +0300

    Disable jQueryUI auto-tooltips

--- dist/js/base.js
+++ dist/js/base.js
@@ -1,3 +1,6 @@
+// Disable jQueryUI auto-tooltips
+jQuery.AdminLTE.options.enableBSToppltip = false;
+
 jQuery(function($){
     // Sidebar menu item activator
     $('section.sidebar li.active').parents('li').addClass('active');
